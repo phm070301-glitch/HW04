b58ae1bccdbf50f620573f122ea2ec1ff7f7ff4a
DESKTOP-NUQCQT0\user <phm070301@gmail.com>
2026-03-25 22:36:17 +0900

    Create HW04 project files

---
+++ HW04/HW04.cpp
@@ -0,0 +1,17 @@
+﻿//원래는 이게 아니겠지만 시간이 없어 여기까지가 한계입니다.
+
+class PotionRecipe 
+{
+public:
+    // 생성자: 이름과 재료 목록을 초기화
+    PotionRecipe(const std::string& name, const std::vector<std::string>& ingredients)
+        : name_(name), ingredients_(ingredients) {
+    }
+
+    const std::string& GetName() const { return name_; }
+    const std::vector<std::string>& GetIngredients() const { return ingredients_; }
+
+private:
+    std::string name_;
+    std::vector<std::string> ingredients_;
+};
